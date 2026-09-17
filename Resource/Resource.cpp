56120cb9e7a9603864895df65170de0acfb99778
bc0723 <benjaminconstantin2006@gmail.com>
2026-09-17 14:55:04 -0500

    Implement default constructor for Resource class
    Added default constructor to initialize Resource fields.

--- Resource/Resource.cpp
+++ Resource/Resource.cpp
@@ -1,1 +1,12 @@
+#include "Resource.h"
+#include <iostream>
+using namespace std;
 
+// Default Constructor (fills every field with a placeholder)
+Resource::Resource()
+{
+  ResourceID = "empty";
+  ResourceName = "empty";
+  ResourceType = "empty";
+  AvailabilityStatus = "Available";
+}
