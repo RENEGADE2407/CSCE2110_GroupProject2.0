455a69c666fd204df30471b3196b1abe351d0d94
bc0723 <benjaminconstantin2006@gmail.com>
2026-09-17 16:02:10 -0500

    Define Resource class in Resource.h
    Added Resource class with constructors and destructor.

--- Resource/Resource.h
+++ Resource/Resource.h
@@ -4,3 +4,24 @@
 #include <string>
 using namespace std;
 
+class Resource
+{
+  private:
+
+  string ResourceID;
+  string ResourceName;
+  string ResourceType;
+  string AvailabilityStatus;
+
+  public:
+
+  // Default constructor
+  Resource();
+
+  // Overloaded constructor
+  Resource(string resourceID, string resourceName, string resourceType, string availabilityStatus);
+
+  // Destructor
+  ~Resource();
+
+  
