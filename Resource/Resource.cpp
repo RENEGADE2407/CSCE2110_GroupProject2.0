90e073561afb6e5a03ee0bed27b788e01ba2b09b
bc0723 <benjaminconstantin2006@gmail.com>
2026-09-17 15:06:02 -0500

    Implement overloaded constructor and destructor in Resource
    Added an overloaded constructor and destructor to the Resource class.

--- Resource/Resource.cpp
+++ Resource/Resource.cpp
@@ -10,3 +10,21 @@ Resource::Resource()
   ResourceType = "empty";
   AvailabilityStatus = "Available";
 }
+// Overloaded Constructor
+Resource:: Resource(string resourceID, string resourceName, string resourceType, string availabilityStatus)
+{
+  this->ResourceID = resourceID;
+  this->ResourceName = resourceName;
+  this->ResourceType = resourceType;
+  this->AvailabilityStatus = availabilityStatus;
+}
+
+//Destructor
+Resource::~Resource()
+{
+
+
+}
+
+
+
