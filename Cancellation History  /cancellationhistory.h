d6d657ec0b2e5ab8ade6d78a667339a7a80861ba
draket321 <drakethompson321@gmail.com>
2026-09-18 02:15:33 -0500

    Update cancellationhistory.h

--- Cancellation History  /cancellationhistory.h
+++ Cancellation History  /cancellationhistory.h
@@ -1,1 +1,28 @@
+#ifndef CANCELLATIONHISTORY_H
+#define CANCELLATIONHISTORY_H
 
+#include "Reservation.h"
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class CancellationHistory
+{
+private:
+
+    // Stack stores cancelled reservations in LIFO order
+    stack<Reservation> cancellationStack;
+
+public:
+
+    // Adds a cancelled reservation to the top of the stack
+    void Push(Reservation reservation);
+
+    // Removes and returns the most recently cancelled reservation
+    bool Pop(Reservation& reservation);
+
+    // Displays all cancelled reservations from most recent to oldest
+    void DisplayHistory() const;
+};
+
+#endif
