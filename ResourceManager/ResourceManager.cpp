cfb6acce2e72a5406d65719bc8d2ff03692beaf0
bc0723 <benjaminconstantin2006@gmail.com>
2026-09-20 04:45:16 -0500

    Add resource loading functionality
    Implemented resource loading from a file with error handling and resource validation.

--- ResourceManager/ResourceManager.cpp
+++ ResourceManager/ResourceManager.cpp
@@ -6,3 +6,127 @@
 using namespace std;
 
 
+// Storage Operations
+Resource::ResourceManager()
+{
+
+
+}
+
+ResourceManager::~ResourceManager()
+{
+
+
+}
+
+bool ResourceManager::LoadResourcesFromFile(string filename)
+{
+  ifstream inputFile(filename);
+
+  if (!inputFile.is_open())
+  {
+    cout << "Error: COuld not open file \"" << filename << "\"." << endl;
+    return false;
+  }
+
+  string line;
+
+  while (getline(inputFile, line))
+  {
+    if (line.empty())
+    {
+      continue;
+    {
+
+    stringstream linestream(line);
+    string resourceID, resourceName, resourceType, availabilityStatus;
+
+    getline(lineStream, resourceID, ',');
+    getline(lineStream, resourceName, ',');
+    getline(lineStream, resourceType, ',');
+
+    if (resourceID.empty() || resourceName.empty() || resourceType.empty() || availabilityStatus.empty())
+    {
+      cout << "Warning: Skipping line in \"" << filename << "\": " << line << endl;
+      continue;
+    {
+
+    Resource newResource(resourceID, resourceName, resourceType, availabilityStatus);
+    AddResource(newResource);
+  }
+
+  inputFile.close();
+  return true;
+}
+
+bool ResourceManager::AddResource(Resource resource)
+{
+  if (ValidateResourceID(resource.GetResourceID()))
+  {
+    cout << "Error: Resource ID \"" << resource.GetResourceID() << "\" already exists." << endl;
+    return false;
+  }
+
+  resources.push_back(resource);
+  return true;
+}
+
+
+void ResourceManager::DisplayResources() const
+{
+  if (resources.empty())
+  {
+    cout << "No resources are currently loaded." << endl;
+    return;
+  }
+
+  cout << "----- All Rsources -----" << endl;
+
+  for (int i = 0; i < resources.size(); i++)
+  {
+    cout << "ID: " << resources[i].GetResourceID()
+         << " | Name: " << resources[i].GetResourceName()
+         << " | Type: " << resources[i].GetREsourceType()
+         << " | Status: " << resources[i].GetAvailabilityStatus() << endl;
+  }
+}
+
+void ResourceManager::DisplayAvailability() const 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
